2047cdf962756b1a1d6857255a65ca1274ac5898
rfamiglietti <romulopfami@gmail.com>
2025-08-05 14:38:22 -0300

    first commit

---
+++ src/components/MeuComponente.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MeuComponente.css';
+
+function MeuComponente() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Este é meu primeiro componente React</p>
+    </div>
+  );
+}
+
+export default MeuComponente;
